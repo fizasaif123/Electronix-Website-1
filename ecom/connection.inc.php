b07c86e7fd322db32e7a5b31c4635974cf0a1d22
fiza saif <62798377+fizasaif123@users.noreply.github.com>
2023-05-31 03:54:06 -0700

    Update connection.inc.php

--- ecom/connection.inc.php
+++ ecom/connection.inc.php
@@ -10,7 +10,7 @@ $con=mysqli_connect("ls-ce1eb672c2222fb22bf65c82d0b2d086c7de3f40.cdb7mxbdnxoe.ap
 define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/Electronix-Website/ecom/');
 
 //Edit the IP address (127.0.0.1) with the Public IP address of your EC2 instance
-define('SITE_PATH','http://2406:da18:f9d:9a00:47f:917:6d37:ca0c/Electronix-Website/ecom/');
+define('SITE_PATH','http://18.143.247.27/Electronix-Website/ecom/');
 
 define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
 define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
